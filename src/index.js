80550ef4a05ef72fb31c6599c565937b56c1cd63
maoznir <nir.maoz@cloudinary.com>
2019-12-22 14:57:19 +0200

    Move constants to a separate file

--- src/index.js
+++ src/index.js
@@ -5,37 +5,14 @@ import 'whatwg-fetch'
 //Use an external janus.
 const { Janus } = require('janus-gateway');
 
-const CLD_API_HOST = "api.cloudinary.com";
-const JANUS_RTC_HOST = "wss://webrtc-api.cloudinary.com/janus";
-
-const JANUS_MESSAGES = {
-  RESULT: "result",
-  ERROR: "error"
-};
-
-const JANUS_MESSAGE_RESULTS = {
-  ID: "id",
-  UPLINK: "uplink",
-  WARNING: "warning",
-  STATUS: "status"
-};
-
-const JANUS_EVENTS = {
-  PREPARING: "preparing",
-  RECORDING: "recording",
-  SLOW_LINK: "slow_link",
-  STOPPED: "stopped"
-};
-
-const EVENTS = {
-  START: "start",
-  STOP: "stop",
-  WARNING: "warning",
-  ERROR: "error",
-  LOCAL_STREAM: "local_stream",
-  REMOTE_STREAM: "remote_stream",
-  DESTROYED: "destroyed"
-};
+import {
+  CLD_API_HOST,
+  JANUS_RTC_HOST,
+  JANUS_MESSAGES,
+  JANUS_MESSAGE_RESULTS,
+  JANUS_EVENTS,
+  EVENTS
+} from './constants';
 
 const buildRequest = (options) => {
   let headers = new Headers();
